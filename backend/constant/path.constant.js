ea733ead4eecb0a0ce83a1cf3f714cc2359909eb
Chandan Kumar Yadav <chandan.yadav@w3villa.com>
2025-07-13 19:33:30 +0530

    amial

--- backend/constant/path.constant.js
+++ backend/constant/path.constant.js
@@ -2,11 +2,11 @@
 const PATHS = {
    SENDER:{
      SERVER: "http://localhost:5173/verification/",
-    LOCALSERVER: "http://localhost:5173/verification/"
+    LOCALSERVER: "https://taskmanagmentmini.vercel.app/verification/"
    },
    CHECKER:{
     SERVER: "http://localhost:4000/verification/",
-    LOCALSERVER: "http://localhost:4000/verification/"
+    LOCALSERVER: "https://taskmanagmentmini.vercel.app/verification/"
    }
 }
 
